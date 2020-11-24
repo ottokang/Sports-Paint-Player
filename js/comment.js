7bb03a450d3aec17b3f263908b90ee39eca98059
ottokang <ottokang@gmail.com>
2020-11-24 17:36:59 +0800

    程式重構

--- js/comment.js
+++ js/comment.js
@@ -1,6 +1,6 @@
 "use strict"
 
-// 設定是否在輸入註解，暫停熱鍵用
+// 設定是否正在輸入註解，暫停熱鍵用
 var isInputComment = false
 
 // 綁定註解檔案上傳動作
@@ -33,20 +33,51 @@ $("#comment_source").on("change", function() {
     commentReader.readAsText(commentFile)
 })
 
-// 綁定關閉註解對話框按鈕動作
+// 綁定點選關閉註解對話框
 $("#close_comment_dialog").on("click", function() {
     closeCommentDialog()
 })
 
-// 綁定開啟新增註解對話框動作
-$("#add_comment").on("click", showNewCommentDialog)
+// 綁定點選顯示新增註解對話框
+$("#add_comment").on("click", function() {
+    isInputComment = true
+    video.pause()
+    // 清除註解對話框內容、建立預設值
+    $("#comment_time_HHMMSS").html("")
+    $("#comment_title_input, #comment_text_input").val("")
+    $("#comment_duration_input").val("10")
 
-// 綁定新增註解提交按鈕動作
-$("#new_comment_submit").on("click", addNewComment)
+    // 顯示新增註解對話框
+    $("#comment_time_HHMMSS").html(video.currentTime.toString().toHHMMSS())
+    $("#comment_dialog").show()
+    $(".new_comment").show()
+    $(".update_comment").hide()
+    $("#comment_position_center").prop("checked", "checked")
+})
 
-// 綁定更新註解提交按鈕動作(ID綁在標題中)
+// 綁定點選新增註解提交按鈕動作
+$("#add_comment_submit").on("click", function() {
+    let comment = loadCommentDialogToJson()
+    // 設定新註解的ID
+    let id
+    if ($("#comment_list > div:last").length === 0) {
+        id = 0
+    } else {
+        id = (parseInt($("#comment_list > div:last").attr("id").replace("comment_item_", "")) + 1).toString()
+    }
+    appendCommentItem(id, comment)
+    showMessage("新增註解成功")
+    closeCommentDialog(false)
+})
+
+// 綁定點選註解更新提交按鈕動作
 $("#update_comment_submit").on("click", function() {
-    updateComment(parseInt($("#update_comment_dialog_title").data("id")))
+    let id = parseInt($("#update_comment_dialog_title").data("id"))
+    let comment = loadCommentDialogToJson()
+    saveCommentJson(id, comment)
+    $(`#comment_item_${id} .comment_title`).html(comment.title)
+    showMessage("更新註解成功")
+    closeCommentDialog(false)
 })
 
 // 顯示註解列表
@@ -74,6 +105,7 @@ function toggleCommentLsit() {
 
 // 顯示註解文字
 function showCommentText(id) {
+    clearAllCommentText()
     let comment = loadCommentJson(id)
     $("#container").append(`<div id="comment_text_${id}" class="comment_text comment_text_${comment.position}">${comment.text}</div>`)
     window.setTimeout(function() {
@@ -90,36 +122,6 @@ function clearAllCommentText() {
     $(".comment_text").remove()
 }
 
-// 顯示新增註解對話框
-function showNewCommentDialog() {
-    isInputComment = true
-    video.pause()
-    // 清除註解對話框內容、建立預設值
-    $("#comment_time_HHMMSS").html("")
-    $("#comment_title_input, #comment_text_input").val("")
-    $("#comment_duration_input").val("10")
-
-    // 顯示新增註解對話框
-    $("#comment_time_HHMMSS").html(video.currentTime.toString().toHHMMSS())
-    $("#comment_dialog").show()
-    $(".new_comment").show()
-    $(".update_comment").hide()
-    $("#comment_position_center").prop("checked", "checked")
-}
-
-// 顯示更新註解對話框
-function showUpdateCommentDialog(id) {
-    let comment = loadCommentJson(id)
-    $("#update_comment_dialog_title").data("id", id)
-    isInputComment = true
-    video.currentTime = comment.time
-    video.pause()
-    loadJsonToCommentDialog(comment)
-    $("#comment_dialog").show()
-    $(".new_comment").hide()
-    $(".update_comment").show()
-}
-
 // 關閉註解對話框
 function closeCommentDialog(isConfirm = true) {
     if (isConfirm === true) {
@@ -133,37 +135,7 @@ function closeCommentDialog(isConfirm = true) {
     }
 }
 
-// 新增註解
-function addNewComment() {
-    let comment = loadCommentDialogToJson()
-    saveComment("new", comment)
-    closeCommentDialog(false)
-}
-
-// 更新註解
-function updateComment(id) {
-    let comment = loadCommentDialogToJson()
-    saveComment(id, comment)
-    $(`#comment_item_${id} .comment_title`).html(comment.title)
-    closeCommentDialog(false)
-}
-
-// 寫入註解到列表
-function saveComment(id, comment) {
-    if (id === "new") {
-        // 找最後一個ID
-        if ($("#comment_list > div:last").length === 0) {
-            id = 0
-        } else {
-            id = (parseInt($("#comment_list > div:last").attr("id").replace("comment_item_", "")) + 1).toString()
-        }
-        appendCommentItem(id, comment)
-    } else {
-        saveCommentJson(id, comment)
-    }
-}
-
-// 附加註解項目到註解選單
+// 附加註解項目到註解列表
 function appendCommentItem(id, comment) {
     $("#comment_list").append(
         `<div id="comment_item_${id}">
@@ -174,19 +146,28 @@ function appendCommentItem(id, comment) {
     )
     saveCommentJson(id, comment)
 
-    // 綁定註解點選動作
+    // 綁定點選跳到註解時間點
     $(`#comment_item_${id} .comment_title`).on("click", function() {
         video.currentTime = loadCommentJson(id).time
-        clearAllCommentText()
         showCommentText(id)
+        video.play()
     })
 
-    // 綁定編輯註解動作
+    // 綁定點選顯示編輯註解對話框
     $(`#comment_item_${id} .edit_comment`).on("click", function() {
-        showUpdateCommentDialog(id)
+        let id = parseInt($(this).parent().attr("id").replace("comment_item_", ""))
+        let comment = loadCommentJson(id)
+        $("#update_comment_dialog_title").data("id", id)
+        isInputComment = true
+        video.currentTime = comment.time
+        video.pause()
+        loadJsonToCommentDialog(comment)
+        $("#comment_dialog").show()
+        $(".new_comment").hide()
+        $(".update_comment").show()
     })
 
-    // 綁定刪除註解動作
+    // 綁定點選刪除註解
     $(`#comment_item_${id} .delete_comment`).on("click", function() {
         if (confirm("確認刪除此註解？")) {
             $(this).parent().remove()
