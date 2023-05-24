4796f9d04ee4acaba273925317133f6b03cd1255
ottokang <ottokang@gmail.com>
2023-05-24 15:01:03 +0800

    調整預設遮罩為較淺的顏色

--- js/obj/draw/polygon.js
+++ js/obj/draw/polygon.js
@@ -86,7 +86,7 @@ var polygon = {
             .replace(/^#?([a-f\d])([a-f\d])([a-f\d])$/i, (m, r, g, b) => "#" + r + r + g + g + b + b)
             .substring(1)
             .match(/.{2}/g)
-            .map((x) => parseInt(x, 16))
+            .map((x) => Number.parseInt(x, 16))
             .join(",");
     },
 };
